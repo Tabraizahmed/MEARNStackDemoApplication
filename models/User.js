a77761896d2731e178cce373cde9fd56d6c0a0cd
tabraiz <ahmed.tabraiz4@gmail.com>
2020-05-10 16:09:43 +0500

    worked on post API. it is complete now

--- models/User.js
+++ models/User.js
@@ -3,24 +3,25 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
+  _id: { type: mongoose.SchemaTypes.ObjectId, required: true, index: true },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   avatar: {
-    type: String
+    type: String,
   },
   date: {
     type: Date,
-    default: Date.Now
-  }
+    default: Date.Now,
+  },
 });
 
 module.exports = User = mongoose.model("user", UserSchema);
